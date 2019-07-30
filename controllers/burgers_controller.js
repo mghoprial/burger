a1a220b86f522b00cf60d04258b443ae18ed158b
mghoprial <50455586+mghoprial@users.noreply.github.com>
2019-07-29 20:39:34 -0700

    html page working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,53 +1,56 @@
 var express = require('express');
+var router = express.Router();
 var burger = require('../models/burger');
 
 router.get("/", function(req, res) {
-    burger.all(function(data) {
-      var hbsObject = {
-        burger: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
+    // burger.all(function(data) {
+    //   var hbsObject = {
+    //     burger: data
+    //   };
+    //   console.log(hbsObject);
+    //   res.render("index", hbsObject);
+    // });
+    res.render("index");
+
   });
   
-  router.post("", function(req, res) {
+  router.post("/burgers/create", function(req, res) {
     burger.create();
   });
   
-  router.put("/api/cats/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
+  // router.put("/api/cats/:id", function(req, res) {
+  //   var condition = "id = " + req.params.id;
   
-    console.log("condition", condition);
+  //   console.log("condition", condition);
   
-    cat.update({
-      sleepy: req.body.sleepy
-    }, condition, function(result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
-  });
+  //   cat.update({
+  //     sleepy: req.body.sleepy
+  //   }, condition, function(result) {
+  //     if (result.changedRows == 0) {
+  //       // If no rows were changed, then the ID must not exist, so 404
+  //       return res.status(404).end();
+  //     } else {
+  //       res.status(200).end();
+  //     }
+  //   });
+  // });
   
-  router.delete("/api/cats/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
+  // router.delete("/api/cats/:id", function(req, res) {
+  //   var condition = "id = " + req.params.id;
   
-    cat.delete(condition, function(result) {
-      if (result.affectedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
-  });
+  //   cat.delete(condition, function(result) {
+  //     if (result.affectedRows == 0) {
+  //       // If no rows were changed, then the ID must not exist, so 404
+  //       return res.status(404).end();
+  //     } else {
+  //       res.status(200).end();
+  //     }
+  //   });
+  // });
+
+
+  module.exports = router;
 
 
 
 
-
-
-var router = express.Router();
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 // Dependencies
 var express = require("express");
-var exphbs = require("express-handlebars");
-
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
+
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
